12a8b28688d100fd73536b70b811b820931948c0
Alphable <327110986@qq.com>
2023-08-17 10:59:41 +0800

    add ESlint

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-undef */
+
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
@@ -11,31 +14,31 @@ app.use(express.static('build'))
 
 // a request&response logger middleware
 // custom a tokon
-morgan.token('body', (request)=> {return JSON.stringify(request.body)})
+morgan.token('body', (request) => {return JSON.stringify(request.body)})
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 
 let persons = [
-    {
-        id: 1,
-        name: "Arto Hellas",
-        number: "040-123456"
-      },
-      {
-        id: 2,
-        name: "Ada Lovelace",
-        number: "39-44-5323523"
-      },
-      {
-        id: 3,
-        name: "Dan Abramov",
-        number: "12-43-234345"
-      },
-      {
-        id: 4,
-        name: "Mary Poppendieck",
-        number: "39-23-6423122"
-      }
+  {
+    id: 1,
+    name: 'Arto Hellas',
+    number: '040-123456'
+  },
+  {
+    id: 2,
+    name: 'Ada Lovelace',
+    number: '39-44-5323523'
+  },
+  {
+    id: 3,
+    name: 'Dan Abramov',
+    number: '12-43-234345'
+  },
+  {
+    id: 4,
+    name: 'Mary Poppendieck',
+    number: '39-23-6423122'
+  }
 ]
 
 /* const generateId = () => {
@@ -43,19 +46,19 @@ let persons = [
 } */
 
 app.get('/info', (request, response) => {
-    response.send(
-        `<div>
+  response.send(
+    `<div>
             Phonebook has info for ${persons.length} people
             <br/>
             ${new Date()}
         </div>`
-    )
+  )
 })
 
 app.get('/api/persons', (request, response) => {
-    Person.find({}).then(people => {
-        response.json(people)
-    })
+  Person.find({}).then(people => {
+    response.json(people)
+  })
 
 })
 
@@ -67,13 +70,13 @@ app.get('/api/persons/:id', (request, response, next) => {
     }else{
         response.json(person)
     } */
-    Person.findById(request.params.id).then(person => {
-        if(person){
-            response.json(person)
-        }else{
-            response.status(404).end()
-        }
-    })
+  Person.findById(request.params.id).then(person => {
+    if(person){
+      response.json(person)
+    }else{
+      response.status(404).end()
+    }
+  })
     .catch(error => next(error))
 })
 
@@ -82,60 +85,60 @@ app.delete('/api/persons/:id', (request, response,next) => {
     persons = persons.filter(one => {return one.id !== id})
     console.log(persons)
     response.status(204).end() */
-    Person.findByIdAndDelete(request.params.id)
+  Person.findByIdAndDelete(request.params.id)
     .then(result => {
-        response.status(204).end()
+      response.status(204).end()
     })
     .catch(error => next(error))
 })
 
 app.post('/api/persons', (request, response, next) => {
-    const body = request.body
-    if(!body.name || !body.number ){
-        return response.status(400).send({error: 'items missing'})
-    }
-    // check if the one already been saved
-    Person.findOne({ name: body.name })
+  const body = request.body
+  if(!body.name || !body.number ){
+    return response.status(400).send({ error: 'items missing' })
+  }
+  // check if the one already been saved
+  Person.findOne({ name: body.name })
     .then(existedPerson => { //人找到 没找到 都是.then的input，而不是.catch
-        console.log(existedPerson)
-        if (existedPerson){// 如果非空
-            console.log('this person [', existedPerson.name, '] already existed.')
-            return response.status(400).send({error:'name must be unique'} )
-        }
-        // continue add person
-        const person = new Person({
-            name: body.name,
-            number: body.number
-        })
-        person.save().then(savedPerson => { // mogodb的保存方法
-            response.json(savedPerson)
-            console.log('person saved!')
-        }).catch(error => next(error))
-        }
+      console.log(existedPerson)
+      if (existedPerson){// 如果非空
+        console.log('this person [', existedPerson.name, '] already existed.')
+        return response.status(400).send({ error:'name must be unique' } )
+      }
+      // continue add person
+      const person = new Person({
+        name: body.name,
+        number: body.number
+      })
+      person.save().then(savedPerson => { // mogodb的保存方法
+        response.json(savedPerson)
+        console.log('person saved!')
+      }).catch(error => next(error))
+    }
     )
 })
 
 app.put('/api/persons/:id', (request, response,next) => {
-    const body = request.body
-    const person = {
-        name: body.name,
-        number: body.number
-    }
-    Person.findByIdAndUpdate(request.params.id,person, {new: true, runValidators: true} )
+  const body = request.body
+  const person = {
+    name: body.name,
+    number: body.number
+  }
+  Person.findByIdAndUpdate(request.params.id,person, { new: true, runValidators: true } )
     .then(returnedPerson => {
-        response.json(returnedPerson)
+      response.json(returnedPerson)
     })
     .catch(error => next(error))
 })
 // 统一处理错误
 const errorHandler = (error, request, response, next) => {
-    console.log(error.message)
-    if (error.name === 'CastError'){
-        return response.status(400).json({error: 'malformatted id'})
-    }else if(error.name === 'ValidationError') {
-        return response.status(400).send({ error: error.message})
-    }
-    next(error)
+  console.log(error.message)
+  if (error.name === 'CastError'){
+    return response.status(400).json({ error: 'malformatted id' })
+  }else if(error.name === 'ValidationError') {
+    return response.status(400).send({ error: error.message })
+  }
+  next(error)
 }
 app.use(errorHandler)
 
